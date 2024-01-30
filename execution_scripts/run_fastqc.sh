fcd3c765ee4686a5006ce6862330af5c7fdceacb
jemilianosf <jemilianosf@gmail.com>
2024-01-30 16:04:08 -0500

    run fastqc

---
+++ execution_scripts/run_fastqc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH -J fastqc
+#SBATCH --mem=2G
+#SBATCH -c 1
+#SBATCH -a 1-1
+
+module load FastQC/0.11.7
+
+names_file=fastq_list.txt
+fastq=$(awk -v i=$SLURM_ARRAY_TASK_ID 'NR==i' $names_file)
+
+fastqc $fastq
+
